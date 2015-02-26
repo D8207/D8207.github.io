1a7a2c2b80a93615bf82a46d631c3218e1faf50d
D8207 <D8207@21cn.com>
2015-02-26 17:43:09 +0800

    Add compat.js

---
+++ crweb/compat.js
@@ -0,0 +1,9 @@
+if ( !Modernizr.inputtypes.number
+	|| !Modernizr.localstorage
+	|| !Modernizr.webworkers
+	|| !Modernizr.inlinesvg
+) {
+	if ( !confirm( '您的浏览器不支持此工具使用的全部技术，是否继续尝试打开？' ) ) {
+		location.href = 'https://whatbrowser.org/';
+	}
+}
